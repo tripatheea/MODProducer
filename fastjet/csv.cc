4b9c3ac8323900a0fe9a4ab6a500d9ec9cee5baa
Aashish <helloworld@aashishtripathee.com>
2015-01-18 10:17:42 -0500

    fastjet code

---
+++ fastjet/csv.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <fstream>
+class CSVRow
+{
+    public:
+        std::string const& operator[](std::size_t index) const
+        {
+            return m_data[index];
+        }
+        std::size_t size() const
+        {
+            return m_data.size();
+        }
+        void readNextRow(std::istream& str)
+        {
+            std::string         line;
+            std::getline(str,line);
+
+            std::stringstream   lineStream(line);
+            std::string         cell;
+
+            m_data.clear();
+            while(std::getline(lineStream,cell,','))
+            {
+                m_data.push_back(cell);
+            }
+        }
+    private:
+        std::vector<std::string>    m_data;
+};
+
+std::istream& operator>>(std::istream& str,CSVRow& data)
+{
+    data.readNextRow(str);
+    return str;
+}  
