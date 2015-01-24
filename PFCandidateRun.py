50337257d7cb4710f195fbb092e2d15dd2188e94
Aashish <helloworld@aashishtripathee.com>
2015-01-24 17:11:25 -0500

    MultiJet Run

--- PFCandidateRun.py
+++ PFCandidateRun.py
@@ -1,4 +1,6 @@
 import FWCore.ParameterSet.Config as cms
+import FWCore.ParameterSet.Config as cms 
+import FWCore.PythonUtilities.LumiList as LumiList 
 
 process = cms.Process("OPENDATA")
 process.load("Configuration.StandardSequences.Geometry_cff")
@@ -10,10 +12,13 @@ process.GlobalTag.globaltag = 'GR_R_42_V25::All'
 readFiles = cms.untracked.vstring()
 process.source = cms.Source ("PoolSource",fileNames = readFiles)
 readFiles.extend( [
-       'root://eospublic.cern.ch//eos/opendata/cms/Run2010B/Mu/AOD/Apr21ReReco-v1/0000/00459D48-EB70-E011-AF09-90E6BA19A252.root'
+       'root://006C7249-7F71-E011-8764-0018F3D09614.root'
 		] );
 
-
+goodJSON = 'Cert_136033-149442_7TeV_Apr21ReReco_Collisions10_JSON_v2.txt' 
+myLumis = LumiList.LumiList(filename = goodJSON).getCMSSWString().split(',') 
+process.source.lumisToProcess = cms.untracked.VLuminosityBlockRange() 
+process.source.lumisToProcess.extend(myLumis)
 
 
 from FWCore.MessageLogger.MessageLogger_cfi import *
