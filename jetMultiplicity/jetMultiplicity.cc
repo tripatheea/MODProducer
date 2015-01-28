b090e569d522cc5fc92a17167f15ed14bea8d891
Aashish <helloworld@aashishtripathee.com>
2015-01-28 16:23:15 -0500

    Jet multiplicity works with multiple events too. Make sure to use C++11

--- jetMultiplicity/jetMultiplicity.cc
+++ jetMultiplicity/jetMultiplicity.cc
@@ -1,5 +1,7 @@
-#include "fastjet/ClusterSequence.hh"
 #include <iostream>
+#include <unordered_map>
+
+#include "fastjet/ClusterSequence.hh"
 #include "csv.cc"
 
 using namespace fastjet;
@@ -9,42 +11,68 @@ double heavisideStep(double x);
 
 double stringToDouble(string s);
 
+
 int main() {
-	std::ifstream file("../PFCandidate.csv");
+	ifstream file("../PFCandidate.csv");
+
+	// Should really be using std::unordered_map as there's absolutely no use of order here and now it's O(logn) access instead of O(1).
+	// Need to compile using C++11 if std::unordered_map is to be used.
+	unordered_map< string, vector<PseudoJet> > events;		
 
 	vector<PseudoJet> particles;
 	double Run, px, py, pz, energy;
+	string event_id;
 
 	double R = 0.5;
 	double pt_cut = 50;
 
 	CSVRow row;
 	while(file >> row) {
+		event_id = row[1];
 		px = stringToDouble(row[2]);
 		py = stringToDouble(row[3]);
 		pz = stringToDouble(row[4]);
 		energy = stringToDouble(row[5]);
-		particles.push_back(PseudoJet(px, py, pz, energy)); 
+		PseudoJet current_particle = PseudoJet(px, py, pz, energy);
+
+		if (events.find(event_id) != events.end()) {	
+			// If we already have a vector of PseudoJets for the current event id, just append to that vector.
+			events[event_id].push_back(current_particle);
+		}
+		else {	
+			// Create a new vector of PseudoJets and insert it with the current event id as the key.
+			vector<PseudoJet> new_particles;
+			new_particles.push_back(current_particle);
+			events[event_id] = new_particles;
+		}
 	}
 
-	double N_tilde_jet = 0.00;
-	for(int i = 0; i < particles.size(); i++) {
-		double pt_i = particles[i].pt();
-		double pt_iR = 0.00;
 
-		if (pt_i == 0.00) continue;			// Removing this line causes a divide by 0 error.
+	vector<double> N_jet;
+
+	for(auto const iter : events) {
 		
-		for(int j = 0; j < particles.size(); j++) {
-			double pt_j = particles[j].pt();
-			double delta_Rij = particles[i].delta_R(particles[j]);
+		vector<PseudoJet> particles_current_event = iter.second;
 
-			pt_iR += pt_j * heavisideStep(R - delta_Rij);
+		double N_jet_current_event = 0.00;
+		for(int i = 0; i < particles_current_event.size(); i++) {
+			double pt_i = particles_current_event[i].pt();
+			double pt_iR = 0.00;
+
+			if (pt_i == 0.00) continue;			// Removing this line causes a divide by 0 error.
+			
+			for(int j = 0; j < particles_current_event.size(); j++) {
+				double pt_j = particles_current_event[j].pt();
+				double delta_Rij = particles_current_event[i].delta_R(particles_current_event[j]);
+
+				pt_iR += pt_j * heavisideStep(R - delta_Rij);
+			}
+
+			N_jet_current_event += pt_i * heavisideStep(pt_iR - pt_cut) / pt_iR;
 		}
 
-		N_tilde_jet += pt_i * heavisideStep(pt_iR - pt_cut) / pt_iR;
+		cout << N_jet_current_event << endl;
 	}
-
-	cout << N_tilde_jet << endl;
 
 }
 
