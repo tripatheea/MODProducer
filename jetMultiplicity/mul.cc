08645828d17c434479757b0fdac1d9b9084279c9
Aashish <helloworld@aashishtripathee.com>
2015-01-28 22:07:38 -0500

    Hist

---
+++ jetMultiplicity/mul.cc
@@ -0,0 +1,43 @@
+#include <iostream>
+#include "csv.cc"
+
+#include <TFile.h>
+#include <TTree.h>
+
+
+using namespace std;
+
+double stringToDouble(string s);
+
+int main() {
+  std::ifstream file("mul.csv");
+
+  TFile * rootFile_;
+  TTree * multiplicityTree_;
+
+  rootFile_ = new TFile("mul", "RECREATE");
+  multiplicityTree_ = new TTree("Multiplicities", "Jet Multiplicities");
+  
+  double mul;
+
+  multiplicityTree_->Branch("multiplicity", &mul, "mul/D");
+
+  CSVRow row;
+  while(file >> row) {
+    mul = stringToDouble(row[0]);
+    multiplicityTree_->Fill();
+  }
+  
+  rootFile_->cd();
+  pfCandidateTree_->Write();
+  rootFile_->Close();
+
+  return 0;
+}
+
+double stringToDouble(string s) {
+  double d;
+  stringstream ss(s); //turn the string into a stream
+  ss >> d; //convert
+  return d;
+}
