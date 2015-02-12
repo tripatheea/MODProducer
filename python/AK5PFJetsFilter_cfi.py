3924aa2e3e6d2b3805af442ae86003301a512025
Aashish <helloworld@aashishtripathee.com>
2015-02-12 01:06:17 -0500

    One event>

--- python/AK5PFJetsFilter_cfi.py
+++ python/AK5PFJetsFilter_cfi.py
@@ -7,5 +7,5 @@ AK5PFJetsFilter = cms.EDFilter('AK5PFJetsFilter' ,
                             AK5PFJetsInputTag = cms.InputTag("ak5PFJets"),
                             csvFileName = cms.string("AK5PFJets.csv"),
                             rootFileName = cms.string("AK5PFJets.root"),
-                            maxNEvents = cms.int32(1000)
+                            maxNEvents = cms.int32(1)
                             )
