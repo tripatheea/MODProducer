628f9feef2ee699211a01b4f391a471a4b970e3d
Aashish <helloworld@aashishtripathee.com>
2015-02-12 01:36:03 -0500

    Back to 10000 events.

--- python/PFCandidateFilter_cfi.py
+++ python/PFCandidateFilter_cfi.py
@@ -7,5 +7,5 @@ PFCandidateFilter = cms.EDFilter('PFCandidateFilter' ,
                             pfCandidateInputTag = cms.InputTag("particleFlow"),
                             csvFileName = cms.string("pfcandidate.csv"),
                             rootFileName = cms.string("pfcandidate.root"),
-                            maxNEvents = cms.int32(1)
+                            maxNEvents = cms.int32(1000)
                             )
