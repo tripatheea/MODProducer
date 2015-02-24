435291dd8537c1f267a5f0083f65199f12d3e5b8
Aashish <helloworld@aashishtripathee.com>
2015-02-24 15:53:26 -0500

    Fixed off by 1 error

--- jetMultiplicity/N_tilde.cc
+++ jetMultiplicity/N_tilde.cc
@@ -17,12 +17,13 @@ int main() {
 
 	int run_number, event_number;
 	
-	int last_event_number = 644983557;
+	int last_event_number = 0;
 	Event * current_event = new Event(149291, 644983557);
 
 	remove("banana.csv");
 
 	int i = 0;
+	int event_count = 1;
 	while(file >> row) {
 
 		run_number = stoi(row[0]);
@@ -34,10 +35,12 @@ int main() {
 
 		PseudoJet current_particle = PseudoJet(px, py, pz, energy);
 
-		current_event->add_particle(px, py, pz, energy);
-
 		if (last_event_number != event_number) {
+
+			// cout << "Processing event #: " << event_count << endl;
 			
+			cout << current_event->size() << endl;
+
 			// We've moved on to a different event.
 			// That means, the class current_event contains all the particles that it's supposed to.
 			
@@ -49,14 +52,18 @@ int main() {
 			// Write current event to a file.
 			current_event->write_to_file("banana.csv");
 
-			// We've calculated all we need. Now delete the pointer and create a new one.
+			// We've calculated all we need. Now delete the old pointer (for the previous instance of event) and create a new one.
 			delete current_event;
 			Event * current_event = new Event(run_number, event_number);
 
 			last_event_number = event_number;
+			event_count++;
 		}
 
+		current_event->add_particle(px, py, pz, energy);
 	}
+
+	// Need to do all this one last time for the final event.
 
 	cout << endl << "Total jets for which N_tilde = 0 is: " << i << endl;
 
