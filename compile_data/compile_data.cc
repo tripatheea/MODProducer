e9eb6a05d922249593fa91e206c09a9775c62df6
Aashish <helloworld@aashishtripathee.com>
2015-05-24 16:10:28 -0400

    ASF

--- compile_data/compile_data.cc
+++ compile_data/compile_data.cc
@@ -18,33 +18,33 @@ int main() {
 
 	int pf_current_event_number = 0;
 
-	// cout.precision(7);
+	cout.precision(7);
 	
-	// while (pfcandidates_file >> pf_run_number >> pf_event_number >> px >> py >> pz >> energy) {
+	while (pfcandidates_file >> pf_run_number >> pf_event_number >> px >> py >> pz >> energy) {
 		
-	// 	if (pf_current_event_number != pf_event_number) {
+		if (pf_current_event_number != pf_event_number) {
 
-	// 		if (pf_current_event_number != 0) {
-	// 			output_file << endl << endl << endl;
-	// 			output_file << "********************  New Event  ********************" << endl << endl;
-	// 			output_file << endl << endl << endl;
-	// 		}
+			if (pf_current_event_number != 0) {
+				output_file << endl << endl << endl;
+				output_file << "********************  New Event  ********************" << endl << endl;
+				output_file << endl << endl << endl;
+			}
 
-	// 		pf_current_event_number = pf_event_number;
+			pf_current_event_number = pf_event_number;
 
-	// 		output_file << "Run Number: " << pf_run_number << endl;
-	// 		output_file << "Event Number: " << pf_event_number << endl << endl;
+			output_file << "Run Number: " << pf_run_number << endl;
+			output_file << "Event Number: " << pf_event_number << endl << endl;
 
-	// 		output_file << "####################  PFCandidates  ####################" << endl << endl;
-	// 		output_file << "    px             py             pz           energy" << fixed << endl;
-	// 	}
+			output_file << "####################  PFCandidates  ####################" << endl << endl;
+			output_file << "    px             py             pz           energy" << fixed << endl;
+		}
 
-	// 	// Now start writing out the 4-vectors.
-	// 	output_file << setprecision(7) << std::showpos << px << "     " << std::showpos << py << "     " << setprecision(7) << std::showpos << pz << "     " << setprecision(7) << std::showpos << energy << endl;
+		// Now start writing out the 4-vectors.
+		output_file << setprecision(7) << std::showpos << px << "     " << std::showpos << py << "     " << setprecision(7) << std::showpos << pz << "     " << setprecision(7) << std::showpos << energy << endl;
 
-	// 	pf_current_event_number = pf_event_number;
+		pf_current_event_number = pf_event_number;
 
-	// }
+	}
 
 	string name;
 	int fired, prescale1, prescale2;
@@ -70,7 +70,7 @@ int main() {
 		}
 
 		// Now start writing out the 4-vectors.
-		output_file << "  " << fired <<  "         " << std::setw(3) << std::setfill('0') << prescale1 << std::setw(3) << std::setfill('0') << "          " << std::setw(3) << std::setfill('0') << prescale2 << "        " << name << endl;
+		output_file << "  " << std::noshowpos << fired <<  "         " << std::setw(3) << std::setfill('0') << std::noshowpos << prescale1 << std::setw(3) << std::setfill('0') << "          " << std::setw(3) << std::setfill('0') << std::noshowpos << prescale2 << "        " << name << endl;
 
 		pf_current_event_number = pf_event_number;
 
