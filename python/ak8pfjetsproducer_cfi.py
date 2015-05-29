f35fe691eea8ed3d4b5af1e355f4e2a572e5ff0c
Aashish <helloworld@aashishtripathee.com>
2015-05-29 01:51:52 -0400

    Ak8

---
+++ python/ak8pfjetsproducer_cfi.py
@@ -0,0 +1,6 @@
+import FWCore.ParameterSet.Config as cms
+
+AK5PFJetsProducer = cms.EDProducer('AK5PFJetsProducer' ,
+                            inputTag = cms.InputTag("ak5PFJets"),
+                            outputFilename = cms.string("AK5PFJets.dat"),
+                            )
