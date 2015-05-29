e70fb35a1f72659ede4bf31bbb3d4164b0014e60
Aashish <helloworld@aashishtripathee.com>
2015-05-29 02:46:26 -0400

    AK8 -> AK7

--- python/ak7pfjetsproducer_cfi.py
+++ python/ak7pfjetsproducer_cfi.py
@@ -1,6 +1,6 @@
 import FWCore.ParameterSet.Config as cms
 
 AK8PFJetsProducer = cms.EDProducer('AK8PFJetsProducer' ,
-                            inputTag = cms.InputTag("ak8PFJets"),
+                            inputTag = cms.InputTag("ak7PFJets"),
                             outputFilename = cms.string("AK8PFJets.dat"),
                             )
