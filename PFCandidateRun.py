5e25595d53597bab8ab5a900b5f38869024db395
Aashish <helloworld@aashishtripathee.com>
2015-06-22 21:48:09 -0400

    Good Lumi

--- PFCandidateRun.py
+++ PFCandidateRun.py
@@ -1,5 +1,6 @@
 import FWCore.ParameterSet.Config as cms
 import FWCore.Utilities.FileUtils as FileUtils
+import FWCore.PythonUtilities.LumiList as LumiList
 import sys
 import os
 from subprocess import call
@@ -25,7 +26,18 @@ readFiles = cms.untracked.vstring()
 readFiles.extend(files_to_process)
 
 process = cms.Process("MITCMSOpenData")
+
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+
+process.GlobalTag.globaltag = 'GR_R_42_V25::All'
+
 process.source = cms.Source ("PoolSource", fileNames=readFiles)
+
+goodJSON = "file_paths/Cert_136033-149442_7TeV_Apr21ReReco_Collisions10_JSON_v2.txt"
+myLumis = LumiList.LumiList(filename = goodJSON).getCMSSWString().split(',')
+process.source.lumisToProcess = cms.untracked.VLuminosityBlockRange()
+process.source.lumisToProcess.extend(myLumis)
+
 process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(1000))
 
 process.PFCandidateProducer = cms.EDProducer("PFCandidateProducer",
